321d8822125351605476bb8d89aa385223833581
maratumerow <maratumerow@gmail.com>
2024-07-16 22:43:01 +0300

    feat: added sentry

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from app.api.routes.users import router
 from app.config import AppConfig
 from app.data.session import db_connect_init
 from app.tools.logging_config import setup_logging
+from app.tools.sentry import sentry_init
 
 
 def get_web_app():
     """Create a FastAPI application."""
 
+    sentry_init()
     db_connect_init()
     config = AppConfig()
     app = FastAPI(title=config.api_title)
